f64ad04325998f340181d6d6281bc78e676f1a54
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-07-05 00:51:57 -0500

    emlpoyee routes terminado

--- src/interfaces/routes/Employee.route.ts
+++ src/interfaces/routes/Employee.route.ts
@@ -1,4 +1,4 @@
-import { createEmployee, getAllEmployee } from "../../interfaces/controllers/Employee.controller";
+import { createEmployee, deleteEmployee, editEmployee, getAllEmployee } from "../../interfaces/controllers/Employee.controller";
 import { Router } from "express";
 const EmployeeRoute = Router()
 
@@ -6,6 +6,7 @@ const EmployeeRoute = Router()
 // Create new Employee
 EmployeeRoute.post("/api/employee", createEmployee)
 EmployeeRoute.get("/api/employee", getAllEmployee)
-
+EmployeeRoute.put("/api/employee", editEmployee)
+EmployeeRoute.delete("/api/employee/:id", deleteEmployee)
 
 export default EmployeeRoute
