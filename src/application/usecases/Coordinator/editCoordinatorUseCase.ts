40af08461447006d0b92d560be32ac0b22556dd1
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-07-06 18:16:57 -0500

    edit coordinator usecase created

--- src/application/usecases/Coordinator/editCoordinatorUseCase.ts
+++ src/application/usecases/Coordinator/editCoordinatorUseCase.ts
@@ -1,8 +1,14 @@
 import { CoordinatorService } from "../../services/Coordinator.serviceInstance";
 import { Coordinator } from "../../../domain/models/Coordinator";
+import bcrypt from "bcrypt"
 
 export async function editCoordinatorUseCase(coordinator: Coordinator) {
     const { cedula } = coordinator
+    const { password } = coordinator
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    coordinator.password = hashedPassword
+
     const isCreated = await CoordinatorService.verificarPorCedula(cedula)
     if (!isCreated) throw new Error(`No existe un coordinador con la cédula ${cedula} imposible editar`)
 
