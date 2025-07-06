5b61d908760f6dfaca1bf8f0a3ac4860fd390c33
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-07-06 18:16:35 -0500

    create coordinator usecase creaed

--- src/application/usecases/Coordinator/createCoordinatorUseCase.ts
+++ src/application/usecases/Coordinator/createCoordinatorUseCase.ts
@@ -1,11 +1,20 @@
 import { CoordinatorService } from "../../services/Coordinator.serviceInstance";
 import { Coordinator } from "../../../domain/models/Coordinator";
+import bcrypt from "bcrypt"
+
 
 export async function createCoordinatorUseCase(coordinator: Coordinator) {
 
     const { cedula } = coordinator
+    const { password } = coordinator
+
     const isCreated = await CoordinatorService.verificarPorCedula(cedula)
     if (isCreated) throw new Error(`Ya existe un coordinador con la cédula ${cedula}`)
+
+    // Hashear password
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    coordinator.password = hashedPassword
 
     const coordinatorCreated = await CoordinatorService.crearCoordinator(coordinator)
     if (!coordinatorCreated) throw new Error("Error al crear el coordinador")
