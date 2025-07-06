0c6e28e50af6afa8ec1fe10b6c0e0b8903ded8b3
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-07-06 18:17:35 -0500

    get all coordinator usecase terminado

--- src/application/usecases/Coordinator/getAllCoordinatorUseCase.ts
+++ src/application/usecases/Coordinator/getAllCoordinatorUseCase.ts
@@ -3,6 +3,7 @@ import { CoordinatorService } from "../../services/Coordinator.serviceInstance";
 export async function getAllCoordinatorUseCase() {
     const coordinatorList = await CoordinatorService.traerCoordinator()
 
+
     return {
         Data: coordinatorList
     }
