55ecc5572c5da1261eecb9fbe818386392d99a90
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-07-07 21:50:10 -0500

    create password bcrypt

--- src/application/usecases/RecursosHumanos/createRecursosHumanosUseCase.ts
+++ src/application/usecases/RecursosHumanos/createRecursosHumanosUseCase.ts
@@ -1,10 +1,14 @@
 import { RecursosHumanosService } from "../../services/RecursosHumanos.serviceInstance";
 import { RecursosHumanos } from "../../../domain/models/RecursosHumanos";
+import bcrypt from "bcrypt"
 
 export async function createRecursosHumanosUseCase(recursosHumanos: RecursosHumanos) {
-    const { cedula } = recursosHumanos
+    const { cedula, password } = recursosHumanos
     const isCreated = await RecursosHumanosService.verificarPorCedula(cedula)
     if (isCreated) throw new Error(`Ya existe un recurso humano con la cédula ${cedula}`)
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    recursosHumanos.password = hashedPassword
 
     const recursosHumanosCreated = await RecursosHumanosService.crearRecursosHumanos(recursosHumanos)
     if (!recursosHumanosCreated) throw new Error("Error al crear el recurso humano")
