83c3c9448cc3638795fa469be38217392e5b5c99
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-07-07 21:50:33 -0500

    Recursos Humanos domain fix

--- src/domain/models/RecursosHumanos.ts
+++ src/domain/models/RecursosHumanos.ts
@@ -1,13 +1,16 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export class RecursosHumanos {
     constructor(
         public cedula: number,
         public nombre: string,
         public cargo: string,
+        public correo: string,
+        public password: string,
         public id?: string,
-        public correo?: string,
-        public password?: string
-    ) { }
-
+    ) {
+        this.id = id ?? uuidv4();
+    }
     validarDatos(): void {
         if (!this.cedula) throw new Error("Falta cédula")
         if (!this.nombre) throw new Error("Falta nombre")
