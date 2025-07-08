f1304d980103b22ff8f435ec6374f9fc7cf54f7a
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-07-07 21:47:46 -0500

    remove auth function Coordinator Controller

--- src/interfaces/routes/Coordinator.route.ts
+++ src/interfaces/routes/Coordinator.route.ts
@@ -1,10 +1,10 @@
-import { createCoordinator, getAllCoordinator, loginCoordinator, editCoordinator, deleteCoordinator } from "../controllers/Coordinator.controller";
+import { createCoordinator, getAllCoordinator, editCoordinator, deleteCoordinator } from "../controllers/Coordinator.controller";
 import { Router } from "express";
 const CoordinatorRoute = Router()
 
 
 CoordinatorRoute.post("/api/coordinator", createCoordinator)
-CoordinatorRoute.post("/api/coordinator/login", loginCoordinator)
+
 CoordinatorRoute.get("/api/coordinator", getAllCoordinator)
 CoordinatorRoute.put("/api/coordinator", editCoordinator)
 CoordinatorRoute.delete("/api/coordinator/:id", deleteCoordinator)
