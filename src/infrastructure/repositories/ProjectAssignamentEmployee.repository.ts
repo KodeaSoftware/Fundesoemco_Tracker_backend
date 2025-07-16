662515686aab1d669cb940fd61869694a2ca21f6
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-07-16 00:15:29 -0500

    ProjectAssignament Reposutory Employee add

---
+++ src/infrastructure/repositories/ProjectAssignamentEmployee.repository.ts
@@ -0,0 +1,34 @@
+import { ProjectAssignamentEmployee } from "../../domain/models/ProjectAssignamentEmployee";
+import { ProjectAssignamentEmployeeModel } from "../persistence/models/ProjectAssignamentEmployeeModel";
+import { ProjectAssignamentEmployeePort } from "../../domain/ports/ProjectAssignamentEmployeePort";
+
+export class ProjectAssignamentEmployeeRepository implements ProjectAssignamentEmployeePort {
+
+    async asignarProyecto(idProject: string, idEmployee: string): Promise<boolean> {
+        try {
+            await ProjectAssignamentEmployeeModel.create({
+                idProject,
+                idEmployee
+            });
+            return true;
+        } catch (error) {
+            throw new Error("Error al asignar proyecto: " + error);
+        }
+    }
+
+    async eliminarEmpleadoDeProyecto(idEmployee: string): Promise<boolean> {
+        const deleted = await ProjectAssignamentEmployeeModel.destroy({ where: { idEmployee } })
+        return deleted > 0
+    }
+
+    async listarEmpleadosDeProyecto(idPorject: string): Promise<ProjectAssignamentEmployee[]> {
+        const asignaciones = await ProjectAssignamentEmployeeModel.findAll({
+            where: { idPorject }
+        });
+        return asignaciones.map(a => new ProjectAssignamentEmployee(
+            a.getDataValue('idPorject'),
+            a.getDataValue('idEmployee'),
+        ));
+    }
+
+}
