052e096062e1955044842ce6933f16c15e9daf84
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-07-19 17:27:07 -0500

    EmployeePort edit

--- src/domain/ports/EmployeePort.ts
+++ src/domain/ports/EmployeePort.ts
@@ -7,10 +7,11 @@
  */
 
 import { Employee } from '../models/Employee';
+import { EmployeeModel } from '../../infrastructure/persistence/models/EmployeeModel';
 
 export interface EmployeePort {
     verificarPorCedula(cedula: number): Promise<boolean>;
-    crearEmpleado(empleado: Employee): Promise<boolean>;
+    crearEmpleado(empleado: Employee): Promise<Employee>;
     eliminarEmpleado(id: string): Promise<boolean>;
     editarEmpleado(empleado: Employee): Promise<boolean>;
     traerEmpleados(): Promise<Employee[]>;
