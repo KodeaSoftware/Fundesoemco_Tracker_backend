a288b41a568e7ada967ba9e6bafa66ac5219372a
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-08-11 10:35:19 -0500

    return project name

--- src/application/usecases/Employee/getAllEmployeeUseCase.ts
+++ src/application/usecases/Employee/getAllEmployeeUseCase.ts
@@ -1,8 +1,30 @@
 import { EmployeeService } from "../../services/Employee.serviceInstance";
+import { ProjectService } from "../../services/Project.serviceInstance";
 
 export async function getAllEmployeeUseCase() {
+
     const employeeList = await EmployeeService.traerEmpleados()
+
+    // Procesar cada empleado para obtener los nombres de los proyectos
+    const employeeListWithProjectNames = await Promise.all(
+        employeeList.map(async (employee) => {
+            // Obtener los nombres de los proyectos para cada empleado
+            const projectNames = await Promise.all(
+                employee.proyecto.map(async (projectId) => {
+                    const project = await ProjectService.traerProjectPorId(projectId)
+                    return project ? project.titulo : 'Proyecto no encontrado'
+                })
+            )
+
+            // Retornar el empleado con los nombres de los proyectos
+            return {
+                ...employee,
+                proyecto: projectNames // Reemplazar los IDs con los nombres
+            }
+        })
+    )
+
     return {
-        Data: employeeList
+        Data: employeeListWithProjectNames
     }
 }
--- src/application/usecases/Employee/getEmployeeByProjectandTypeContractUseCase.ts
+++ src/application/usecases/Employee/getEmployeeByProjectandTypeContractUseCase.ts
@@ -1,5 +1,5 @@
 import { EmployeeService } from "../../services/Employee.serviceInstance"
-import { ProjectService } from "../../services/Project.serviceInstance"
+
 
 export async function getEmployeeByProjectandTypeContractUseCase(idProject: string, tipoContrato: number) {
     const employeeList = await EmployeeService.traerPorProyectoContrato(idProject, tipoContrato)
